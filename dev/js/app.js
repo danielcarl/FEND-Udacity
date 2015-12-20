588ceb3d2fca39e7d0065103cc9a1d25f2d40fa1
Daniel Carl <danielcarl@Daniels-MacBook-Pro.local>
2015-12-20 17:50:30 -0600

    Modified app.js to use module pattern; added bootstrap to allow for responsive layout

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,61 +1,59 @@
-// TODO: Set up ModelView and display text on page (no map)
+var NeighborhoodMap = function () {
+	
+	var neighborhood = {
+		name: ko.observable('Bouldin'),
+		city: ko.observable('Austin'),
+		state: ko.observable('TX'),
+		zip: ko.observable(78704),
+		latLng: {lat: 30.255, lng: -97.755},
+	};
 
-// Constructor for points of interst
-function PointOfInterest(name, latitude, longitude, category) {
-
-	var self = this;
-	self.name = name;
-	self.latitude = latitude;
-	self.longitude = longitude;
-	self.category = category;
-
-	// creates a letLng object, to be used with the Google Map API
-	self.latLng = ko.computed(function() {
-
-		latLngObject = {
-			latitude,
-			longitude
-		}
-
-		console.log(latLngObject);
-		return latLngObject
-
-	}, self);
-
-}
-
-function neighborhoodViewModel() {
-
-  	var self = this;
-
-  	self.name = ko.observable('Bouldin');
-  	self.city = ko.observable('Austin');
-  	self.state = ko.observable('TX');
-  	self.zip = ko.observable(78704);
-  	self.latLng = {lat: 30.255, lng: -97.755};
-
-  	self.pointsOfInterest = ko.observableArray([
+	var pointsOfInterest = ko.observableArray([
 
   		new PointOfInterest('Dominican Joe', 30.2561064, -97.7469545, 'coffee shops'),
     	new PointOfInterest('South Congress Books', 30.2504842, -97.7526313, 'book stores'),
 		new PointOfInterest('Elizabeth Street Cafe', 30.2471979, -97.7556907, 'restaurants'),
     	new PointOfInterest('Alamo Drafthouse', 30.256403, -97.7634876, 'entertainment'),
+    	// new PointOfInterest('Buzz Mill', 30.2600000, -97.7400000, 'coffee shops'),
+    	// new PointOfInterest('Chupacapra', 30.2600000, -97.7400000, 'nightlife'),
+    	// new PointOfInterest('Whip In', 30.2600000, -97.7400000, 'restaurants'),
     	new PointOfInterest('South Congress Cafe', 30.2504842, -97.7526313, 'restaurants')
 
 	]);
 
-	self.cityAndState = ko.computed(function() {
+	function PointOfInterest(name, latitude, longitude, category) {
 
-  		return self.city() + ', ' + self.state();
+		var self = this;
+		self.name = name;
+		self.latitude = latitude;
+		self.longitude = longitude;
+		self.category = category;
 
-  	}, self);
+		// creates a letLng object, to be used with the Google Map API
+		self.latLng = ko.computed(function() {
 
-}
+			latLngObject = {
+				latitude,
+				longitude
+			}
 
-// Activates knockout.js
-ko.applyBindings(new neighborhoodViewModel());
+			console.log(latLngObject);
+			return latLngObject
 
-// TODO: Add observables
-// TODO: Add Filtering search
-// TODO: Add map
-// TODO: Add marker data
+		}, self);
+
+	}
+
+	var init = function () {
+		ko.applyBindings(NeighborhoodMap);
+	};
+
+	$(init);
+
+	return {
+		neighborhood: neighborhood,
+		pointsOfInterest: pointsOfInterest,
+		PointOfInterest: PointOfInterest
+	};
+
+}();
