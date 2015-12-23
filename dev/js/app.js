308c202bdec40e890270f47b0b7a343ab0244b14
Daniel Carl <dan.carl@heurekasoftware.com>
2015-12-22 18:30:24 -0600

    Modified search to only match place names from the beginning of the string; moved all model data out of the viewModel so that it can be accessed by map functions; removed hard-coded map latLng from initMap() function, and instead pull from the neighborhood model; added ko.util.stringStartsWith() function, since it is not included in the minified version of Knockout.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,11 @@
+var neighborhood = {
+	"name" : "South Congress",
+	"city" : "Austin",
+	"state" : "TX",
+	"postalCode" : 78704,
+	"latLng" : {lat: 30.255, lng: -97.755}
+};
+
 var places = [
 	{"name" : "Alamo Drafthouse", "category" : "Entertainment", "latLng" : {lat: 30.256403, lng: -97.7634876}},
 	{"name" : "Buzzmill", "category" : "Coffee Shops", "latLng" : {lat: 30.2417288, lng: -97.7290856}},
@@ -8,22 +16,39 @@ var places = [
 	{"name" : "Tribe Comics And Games", "category" : "Entertainment", "latLng" : {lat: 30.2414233, lng: -97.7863233}}
 ];
 
+// This function is not included in the minified version of Knockout, so I included it here.
+ko.utils.stringStartsWith = function (string, startsWith) {        	
+    string = string || "";
+    if (startsWith.length > string.length)
+        return false;
+    return string.substring(0, startsWith.length) === startsWith;
+};
+
 var neighborhoodViewModel = function() {
 
 	var self = this;
 
-	self.neighborhoodName = ko.observable("South Congress");
-	self.neighborhoodCity = ko.observable("Austin");
-	self.neighborhoodState = ko.observable("TX");
-	self.neighborhoodPostalCode = ko.observable(78704);
-	self.latLng = {lat: 30.255, lng: -97.755};
+	// Creates observables for neighborhood
+	self.neighborhoodName = ko.observable(neighborhood.name);
+	self.neighborhoodCity = ko.observable(neighborhood.city);
+	self.neighborhoodState = ko.observable(neighborhood.state);
+	self.neighborhoodPostalCode = ko.observable(neighborhood.postalCode);
+
+	// Creates observable array for points of interest; used to create searchable array
 	self.pointsOfInterest = ko.observableArray(places);
 
+	// Creates a computed string for city, state, and zip.
+	self.neighborhoodCityState = ko.computed(function() {
+		return self.neighborhoodCity() + ', ' + self.neighborhoodState() + ' ' + self.neighborhoodPostalCode();
+	}, this);
+
+	// Search filter for point of interst list. Modified from http://codepen.io/JohnMav/pen/OVEzWM/ -
+	// my version matches from the beginning of the string only.
 	self.query = ko.observable('');
 
 	self.search = ko.computed(function() {
-		return ko.utils.arrayFilter(self.pointsOfInterest(), function(point){
-			return point.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+		return ko.utils.arrayFilter(self.pointsOfInterest(), function(pointOfInterest){
+			return ko.utils.stringStartsWith(pointOfInterest.name.toLowerCase(), self.query().toLowerCase());
 		});
 	});
 };
@@ -31,12 +56,10 @@ var neighborhoodViewModel = function() {
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 30.255, lng: -97.755},
-    zoom: 14
-  });
+  	map = new google.maps.Map(document.getElementById('map'), {
+		center: neighborhood.latLng,
+    	zoom: 14
+  	});
 }
-
-
 
 ko.applyBindings(new neighborhoodViewModel());
